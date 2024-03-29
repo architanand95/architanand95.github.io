d0ca1b0c25c05c44adf07eba8e6d6abc0a8dadc0
Archit Anand <106612899+architanand95@users.noreply.github.com>
2024-03-29 22:05:49 +0530

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -29,10 +29,7 @@ const Navigation = () => {
         <a onClick={() => handleNavigate('/')}>
           <img src="/favicon.ico" alt="Custom Icon" className={styles.navIcon} />
         </a>
-        <div className="flex items-center">
-          {/* <button className={styles.navButton} onClick={() => handleNavigate('/')}>
-            <FaHome /> Home
-          </button> */}
+        <div className={style.navmudda}>
           <button className={styles.navButton} onClick={() => handleNavigate('/about')}>
             <FaUser /> About
           </button> 
