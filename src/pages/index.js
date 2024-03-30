199e57ed0a62c133e27a2a71ca154db4881fdae6
Archit Anand <106612899+architanand95@users.noreply.github.com>
2024-03-30 17:05:47 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -269,8 +269,8 @@ export default function Home() {
         </button>
         {/* About Section */}
         <section className={styles.section}>
-          <div className={styles.container} id="about">
-            <h1 className={styles.subHeading} >About Me</h1>
+          <div className={styles.container}>
+            <h1 className={styles.subHeading id="about"} >About Me</h1>
             <div className={styles.aboutContent} >
               <div className={styles.aboutLeft}>
                 {/* Placeholder image, replace with your own */}
